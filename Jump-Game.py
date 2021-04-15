039df4d2f6ed8e70e41f0456e641b3fef03a68c1
Jathurchan Selvakumar <mail@jathurchan.com>
2021-04-15 11:00:24 +0200

    Greedy Solution

--- Jump-Game.py
+++ Jump-Game.py
@@ -9,22 +9,10 @@ def canJump(nums):
 
     n = len(nums)
 
-    mem = [-1] * n  # -1 : unknown position, 0 : bad position and 1 : good position (can reach the last index from)
-    mem[n-1] = 1     # last index is obviously a good position.
+    last_i = n - 1  # last index
 
-    for i in range(n-1):
-
-        curr_i = n - 2 - i  # start from the right side
-        max_jump_len = nums[curr_i]
-
-        for k in range(1, max_jump_len + 1):
-
-            next_i = curr_i + k
-            if next_i < n and mem[next_i] == 1:
-                mem[curr_i] = 1
-                break
-        
-        if mem[curr_i] == -1:
-            mem[curr_i] = 0
-
-    return mem[0] == 1
+    for curr_i in range(n-2, -1, -1):
+        if (curr_i + nums[curr_i] >= last_i):
+            last_i = curr_i
+    
+    return last_i == 0
