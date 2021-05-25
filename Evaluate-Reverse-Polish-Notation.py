5ce4ce079c8abe2e1676374a58352ce0a94782c5
Jathurchan Selvakumar <mail@jathurchan.com>
2021-05-25 11:26:56 +0200

    Success
    Runtime: 60 ms, faster than 92.42% of Python3 online submissions for Evaluate Reverse Polish Notation.
    Memory Usage: 14.4 MB, less than 96.45% of Python3 online submissions for Evaluate Reverse Polish Notation.

--- Evaluate-Reverse-Polish-Notation.py
+++ Evaluate-Reverse-Polish-Notation.py
@@ -19,59 +19,24 @@ class Solution(object):
         :rtype: int
         """
 
-        n = len(tokens)
+        stack = []
 
-        if n == 1:
-            return int(tokens[0])
+        ops = ["+", "-", "*", "/"]
 
-        ops = ['+', '-', '*', '/']
-
-        def compute(l, r, o):
-            if o == '+':
-                return l + r
-            elif o == '-':
-                return l - r
-            elif o == '*':
-                return l * r
+        for elt in tokens:
+            if elt not in ops:
+                stack.append(int(elt))
             else:
-                return int(l / r)
-        
-        accu = []
-        prev = []
-        lft, rgt = 0, 0
-        
-        for i in range(n):
-            if tokens[i] in ops:    # operator?
-
-                op = tokens[i]
-
-                if (tokens[i-1] not in ops) and (tokens[i-2] not in ops):
-                    rgt = int(tokens[i-1])
-                    lft = int(tokens[i-2])
-                    accu.append(compute(lft, rgt, op))
-                    if i > 2 and tokens[i-3] not in ops:
-                        prev.append(i-3)
-                elif (tokens[i-1] in ops):
-                    rgt = accu.pop()
-                    if prev:
-                        pre = prev.pop()
-                        lft = int(tokens[pre])
-                        if pre > 0 and tokens[pre-1] not in ops:
-                            prev.append(pre-1)
-                    else:
-                        lft = accu.pop()
-                    accu.append(compute(lft, rgt, op))
-                else:   # tokens[i-2] in ops and tokens[i-1] not in ops
-                    lft = accu.pop()
-                    rgt = int(tokens[i-1])
-                    accu.append(compute(lft, rgt, op))
-
-                print(i)
-                print(accu)
-                print(prev)
-
-        
-        return sum(accu)
+                rgt, lft = stack.pop(), stack.pop()
+                if elt == "+":
+                    stack.append(rgt+lft)
+                elif elt == "-":
+                    stack.append(lft-rgt)
+                elif elt == "*":
+                    stack.append(lft*rgt)
+                else:
+                    stack.append(int(float(lft)/rgt))
+        return stack.pop()
 
 sol = Solution()
 print(sol.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
