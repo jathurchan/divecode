a0f29d1a61505fd2e8462990c3b749d011ceb1f8
Jathurchan Selvakumar <admin@jathurchan.com>
2023-10-22 12:52:01 +0200

    Removed previous work

--- 2020-2022/prev/1217_MinimumCosttoMoveChipstoTheSamePosition.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minCostToMoveChips(self, position):
-        a, b = 0, 0
-        for p in position:
-            if p % 2 == 0:
-                a += 1
-            else:
-                b += 1
-        return min(a, b)
