d628be4b40e67a4fcccba1a3fd1ebd96c54cd82a
Jathurchan Selvakumar <admin@jathurchan.com>
2023-10-23 16:43:28 +0200

    Solved 342. Power of Four (follow up)

--- solutions/python/00342-power-of-four.py
+++ solutions/python/00342-power-of-four.py
@@ -5,3 +5,14 @@ class Solution:
         while n % 4 == 0:
             n /= 4
         return n == 1
+    
+    def isPowerOfFour2(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        if n & (n-1) != 0:  # n is not a power of 2
+            return False
+        
+        power4Positions = 0b01010101010101010101010101010101    # 32 bits
+        res = (power4Positions | n == power4Positions) # power of 4?
+        return res
